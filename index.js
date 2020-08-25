92a83e3a0d3fabcc611fc2742f9e1c01311df716
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-25 08:05:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+class Cat {
+  constructor(name,sex) {
+    this.name=name;
+    this.sex=sex;
+  }
+}
+
+class Dog {
+  onstructor(name,sex) {
+    this.name=name;
+    this.sex=sex;
+  }
+}
+
+class Bird {
+  onstructor(name,sex) {
+    this.name=name;
+    this.sex=sex;
+  }
+}
